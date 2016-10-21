a524d94fb31cd8768f9cc5f58758221dceaa23d6
Alvaro Flores <alvaro.flores01@gmail.com>
2016-10-21 09:48:51 -0700

    added more components to site

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <h1>Track your every meal</h1>
+  <div class="meal" *ngFor="let meal of meals">
+  <h2>{{ meal.name }}</h2>
+  <h2>{{ meal.details }}</h2>
+  <h2>{{ meal.calories }} calories</h2>
+  </div>
   `
 })
 
 export class AppComponent {
-
+  meals: Meal[] = [
+    new Meal("Burger", "ate half of it", 145),
+  ];
+}
+export class Meal {
+  constructor(public name: string, public details: string, public calories: number){ }
 }
